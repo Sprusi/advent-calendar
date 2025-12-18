0c0a8bcb60258631e08ac3681f256a2e58236000
brovko.dr <brovko@atb.su>
2025-12-19 01:21:39 +1000

    added deploy settings

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <ConfigProvider locale={ru_RU}>
       <ErrorBoundary>
-        <BrowserRouter>
+        <BrowserRouter basename="/advent-calendar/">
           <Navigation />
         </BrowserRouter>
       </ErrorBoundary>
